23d83c82e26fc270ee8b940472f219446e5fbfc4
Balaji Jayaraman <balaji.jayaraman@airbus.com>
2020-07-29 19:58:12 +0530

    gltf loader added

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ import * as facemesh from '@tensorflow-models/facemesh';
 import Stats from 'stats.js';
 import * as tf from '@tensorflow/tfjs-core';
 import * as tfjsWasm from '@tensorflow/tfjs-backend-wasm';
-import * as three from 'three.js';
+import * as THREE from 'three';
+import GLTFLoader from 'three-gltf-loader';
 // TODO(annxingyuan): read version from tfjsWasm directly once
 // https://github.com/tensorflow/tfjs/pull/2819 is merged.
 import {version} from '@tensorflow/tfjs-backend-wasm/dist/version';
@@ -34,7 +35,7 @@ function isMobile() {
   return isAndroid || isiOS;
 }
 
-let model, ctx, videoWidth, videoHeight, video, canvas;
+let model, ctx, videoWidth, videoHeight, video, canvas, scene, camera, renderer;
 
 const VIDEO_SIZE = 500;
 const mobile = isMobile();
@@ -155,4 +156,48 @@ async function main() {
   renderPrediction();
 };
 
+function initThreeJS() {
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xdddddd);
+  camera = new THREE.PerspectiveCamera(40,window.innerWidth/window.innerHeight,1,5000);
+  camera.rotation.y = 45/180*Math.PI;
+  camera.position.x = 800;
+  camera.position.y = 100;
+  camera.position.z = 1000;
+  var hlight = new THREE.AmbientLight (0x404040,100);
+  scene.add(hlight);
+  var directionalLight = new THREE.DirectionalLight(0xffffff,100);
+  directionalLight.position.set(0,1,0);
+  directionalLight.castShadow = true;
+  scene.add(directionalLight);
+  var light = new THREE.PointLight(0xc4c4c4,10);
+  light.position.set(0,300,500);
+  scene.add(light);
+  var light2 = new THREE.PointLight(0xc4c4c4,10);
+  light2.position.set(500,100,0);
+  scene.add(light2);
+  var light3 = new THREE.PointLight(0xc4c4c4,10);
+  light3.position.set(0,100,-500);
+  scene.add(light3);
+  var light4 = new THREE.PointLight(0xc4c4c4,10);
+  light4.position.set(-500,300,500);
+  scene.add(light4);
+  var renderer = new THREE.WebGLRenderer({antialias:true});
+  renderer.setSize(window.innerWidth,window.innerHeight);
+  document.getElementById("threejs-container").appendChild(renderer.domElement);
+  let loader = new GLTFLoader();
+  loader.load("models/necklace.glb", function(gltf){
+    model = gltf.scene;
+    model.scale.set(0.5,0.5,0.5);
+    scene.add(gltf.scene);
+    animate();
+  });
+}
+
+function animate() {
+  renderer.render(scene,camera);
+  requestAnimationFrame(animate);
+}
+
 main();
+initThreeJS();
